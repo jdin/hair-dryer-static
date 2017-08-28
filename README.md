05cef7a4c9be239c08b1af03d541586b4fec5373
skondratenko <skondratenko@asset-control.com>
2017-08-28 17:57:33 +0200

    vue js and working sound

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 `npm install`
 
 `npm start`
+
+work in progress ...
--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,33 @@
 (function() {
-console.log("loaded");
+    console.log("loaded");
+
+    var sound = new Howl({
+        src : ['data/out.ogg'],
+        loop : 1
+    });
+
+    if (localStorage) {
+        localStorage.sound = sound;
+    }
+
+    var app = new Vue({
+        el : "#app",
+        data : {
+            isStarted : false
+        },
+        methods : {
+            getButtonName : function() {
+                return this.isStarted ? "Stop" : "Start";
+            },
+            startStop : function() {
+                this.isStarted = !this.isStarted;
+                if (this.isStarted) {
+                    sound.play();
+                } else {
+                    sound.stop();
+                }
+            }
+        }
+    });
+
 })();
